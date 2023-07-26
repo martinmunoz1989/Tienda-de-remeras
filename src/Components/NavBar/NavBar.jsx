34120dabc516fd450a35e145db0e4ec8dee7fb6f
Martin Muñoz <mmvdp89@gmail.com>
2023-07-25 22:05:44 -0300

    modifico el navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,18 +1,41 @@
 import CartWidget from "../CartWidget/CartWidget.jsx";
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
 const NavBar = () => {
     return (
-        <nav>
+        <>
             <h1>Tienda de Camisetas de Futbol</h1>
-            <div>
-                <Button variant="primary" className="me-2">Liga Argentina</Button>
-                <Button variant="primary"className="me-2">Sudamerica</Button>
-                <Button variant="primary"className="me-2">Europa</Button>
-                <Button variant="primary"className="me-2">Contacto</Button>
-            </div>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#Argentina">Nacionales</Nav.Link>
+                        <Nav.Link href="#Europa">Internacionales</Nav.Link>
+                        <Nav.Link href="#pricing">Contacto</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
             <CartWidget />
-        </nav>
-    )
+        </>
+    );
 }
 
 export default NavBar;
+
+/* return (
+     <nav>
+         <h1>Tienda de Camisetas de Futbol</h1>
+         <div>
+             <Button variant="primary" className="me-2">Liga Argentina</Button>
+             <Button variant="primary" className="me-2">Sudamerica</Button>
+             <Button variant="primary" className="me-2">Europa</Button>
+             <Button variant="primary" className="me-2">Contacto</Button>
+         </div>
+         <CartWidget />
+     </nav>
+ )*/
+
+
