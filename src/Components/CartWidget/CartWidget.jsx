f128b261ff56ed5002418108302b97d095635de5
Martin Muñoz <mmvdp89@gmail.com>
2023-08-22 21:35:50 -0300

    agrego badge al cart

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react'
 import '../../assets/CartWidget.css'
 import cart from './assets/cart.svg'
+import { ItemsContext } from '../../context/CartContext'
+import { Badge } from 'react-bootstrap'
 const CartWidget = () => {
+    const CartContext = useContext(ItemsContext)
+
+    console.log({ CartContext });
     return <div>
         <img src={cart} alt="cart-widget" id="cart-widget" />
-        0
+        <Badge>3</Badge>
+        
     </div>
 }
 
