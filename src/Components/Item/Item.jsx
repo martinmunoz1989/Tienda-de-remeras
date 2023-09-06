96c8a706ad52114c78eeb6a22078c19e0c68b5e8
Martin Muñoz <mmvdp89@gmail.com>
2023-09-05 23:33:49 -0300

    agrego toast

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,12 +1,15 @@
 import Card from 'react-bootstrap/Card';
 import ItemCount from '../ItemCount/itemCount';
-import { Button } from 'react-bootstrap';
+import { Button, Toast } from 'react-bootstrap';
 import ItemDetail from '../ItemDetail/ItemDetail';
 import { useState } from 'react';
+
 
 function Articulo({ id, imagen, descripcion, precio }) {
     // para al hacer click manejar el evento
     const [verDetalle, setVerDetalle] = useState(false)
+    const [showToast, setShowToast] = useState(false);
+    const [cantAgregada, setCantAgregada] = useState(0);
 
     return (
         <Card key={id} style={{ width: '18rem' }}>
@@ -15,10 +18,32 @@ function Articulo({ id, imagen, descripcion, precio }) {
                 {!verDetalle && (
                     <Button variant="dark" onClick={() => setVerDetalle(true)}>Ver detalle</Button>
                 )}
-                {verDetalle && <ItemDetail id={id} descripcion={descripcion} precio={precio}  />}
-                <ItemCount onAdd={(contador) => alert("Has agregado " + contador + " unidades al carrito")} />
+                {verDetalle && <ItemDetail id={id} descripcion={descripcion} precio={precio} />}
+                <ItemCount onAdd={(contador) => {
+                    setShowToast(true);
+                    setCantAgregada(contador);
+                }} />
             </Card.Body>
-        </Card>
+            <Toast
+                onClose={() => setShowToast(false)}
+                show={showToast}
+                delay={3000}
+                autohide
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: '50%',
+                    transform: 'translateX(-50%)'
+                }}
+            >
+                <Toast.Header>
+                    <strong className="me-auto">Notificación</strong>
+                </Toast.Header>
+                <Toast.Body>Has agregado {cantAgregada} unidades al carrito</Toast.Body>
+            </Toast>
+
+
+        </Card >
     );
 }
 
--- src/Components/ItemCount/itemCount.jsx
+++ src/Components/ItemCount/itemCount.jsx
@@ -2,9 +2,11 @@ import { Button } from "react-bootstrap";
 import { useContext, useState } from "react";
 import { ItemsContext } from "../../context/CartContext";
 
+
 function ItemCount({ onAdd }) {
     const CartContext = useContext(ItemsContext);
     const [contador, setContador] = useState(0);
+
 
     const sumar = () => {
         setContador(contador + 1);
@@ -16,7 +18,7 @@ function ItemCount({ onAdd }) {
         }
     }
 
-    const agregarAlCarrito =() =>{
+    const agregarAlCarrito = () => {
         CartContext.sumarItemCant(contador);
         onAdd(contador);
     }
@@ -29,7 +31,7 @@ function ItemCount({ onAdd }) {
                 <h6 className="d-flex align-items-center mx-2">{contador} </h6>
                 <Button variant="dark" onClick={sumar}>+</Button>
             </div>
-            <Button variant="dark" className='my-2' onClick={agregarAlCarrito}>Agregar al carrito</Button>;
+            <Button variant="dark" className='my-2' onClick={agregarAlCarrito}>Agregar al carrito</Button>
         </>
     );
 }
