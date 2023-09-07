df1f9b6f7999642c727142cd07f9ea26013ff79b
Martin Muñoz <mmvdp89@gmail.com>
2023-09-06 23:37:59 -0300

    agrego y termino el form con datos tomados del usuario y sin usar firestore

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,11 +1,38 @@
-import CheckoutForm from "../CheckoutForm/CheckoutForm.jsx"
-import NavBar from "../NavBar/NavBar.jsx"
+import { useState, useContext } from 'react'
+import { ItemsContext } from '../../context/CartContext'
+import CheckoutForm from '../CheckoutForm/Checkoutform'
+import NavBar from '../NavBar/NavBar'
 
 const Checkout = () => {
-    return <>
-        <NavBar />
-        <CheckoutForm />
-    </>
+    const [orderId, setOrderId] = useState('')
+    const { cart, total, clearCart } = useContext(ItemsContext)
+
+    const createOrder = ({ name, phone, email }) => {
+        // Simplemente imprimimos los datos para visualizarlos. 
+        // Puedes hacer cualquier otra lógica que necesites con estos datos.
+        console.log('Datos del formulario:', { name, phone, email, cart, total });
+
+        // Por simplicidad, generamos un ID de orden aleatorio.
+        const generatedOrderId = `ORD-${Math.floor(Math.random() * 1000000)}`;
+        setOrderId(generatedOrderId);
+
+        clearCart();  // Si deseas limpiar el carrito después de crear la orden.
+    }
+
+    if (orderId) {
+        return <>
+            <NavBar />
+            <h1>El código de seguimiento de su orden es: {orderId}</h1>
+        </>
+    }
+
+    return (
+        <div>
+            <NavBar />
+            <h1>Checkout</h1>
+            <CheckoutForm onConfirm={createOrder} />
+        </div>
+    );
 }
 
-export default Checkout
+export default Checkout;
